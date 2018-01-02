4aa5e8a03a25c78f8b8f789b6fd9619fa571eada
Miles McLeod <milesmcleod123@gmail.com>
2018-01-02 17:31:15 -0300

    Initialize directory

---
+++ firstname_lastname 6/lib/dynamic_array.rb
@@ -0,0 +1,54 @@
+require_relative "static_array"
+
+class DynamicArray
+  attr_reader :length
+
+  def initialize
+    @store = StaticArray.new(2)
+    @capacity = 2
+    @length = 0
+  end
+
+  # O(1)
+  def [](index)
+    @store[index]
+  end
+
+  # O(1)
+  def []=(index, value)
+    @store[index] = value
+  end
+
+  # O(1)
+  def pop
+
+  end
+
+  # O(1) ammortized; O(n) worst case. Variable because of the possible
+  # resize.
+  def push(val)
+  end
+
+  # O(n): has to shift over all the elements.
+  def shift
+  end
+
+  # O(n): has to shift over all the elements.
+  def unshift(val)
+  end
+
+  protected
+  attr_accessor :capacity, :store
+  attr_writer :length
+
+  def check_index(index)
+  end
+
+  # O(n): has to copy over all the elements to the new store.
+  def resize!
+    new_store = StaticArray.new(self.capacity * 2)
+    i = 0
+    while i < self.length
+      
+  end
+end
